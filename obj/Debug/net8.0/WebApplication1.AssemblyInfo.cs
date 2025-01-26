7f09e891d41e484e57f2f87f70d48e921f0630fc
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-01-26 12:21:48 +0530

    v6

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124bdcaaa9813ca7981561e14da4762123d3b846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a6dcc99a98c35ae5435dba0a93bca4d1571183")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
